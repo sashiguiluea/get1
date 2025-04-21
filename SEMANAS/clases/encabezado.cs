6e8dff26e223679dec5ebc2b8c4127832b2bef67
SEGUNDO ANDRES SHIGUI LEMA <sa.shiguil@uea.edu.ec>
2025-04-21 16:56:54 -0500

    Descripción de los cambios

--- SEMANAS/clases/encabezado.cs
+++ SEMANAS/clases/encabezado.cs
@@ -3,7 +3,7 @@ public class encabezado
         public static void MostrarEncabezado()
         {
             Console.WriteLine("-----UNIVERSIDAD ESTATAL AMAZONICA-----");
-            Console.WriteLine("---------ESTRUCTURA DE DATOS-----------");
+            Console.WriteLine("---------ESTRUCTURA DE DATOS 2025-----------");
         }
 
     }
